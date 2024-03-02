7ed08ffbd7ff78a9fead4ce3e2a05481c9622c27
Abderrazak LAANAYA <laanayabdrzak@gmail.com>
2024-03-02 19:04:51 +0100

    Initial Commit

---
+++ PhoneCallSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhoneCallSwiftUI
+//
+//  Created by LAANAYA Abderrazak on 2/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhoneCallSwiftUI/PhoneCallSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneCallSwiftUIApp.swift
+//  PhoneCallSwiftUI
+//
+//  Created by LAANAYA Abderrazak on 2/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct PhoneCallSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
